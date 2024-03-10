b547babe7e0a6f7036998712cbcf5ed6718df131
mkshing <mktshing4@gmail.com>
2024-03-10 14:55:06 +0900

    test

--- vllm/worker/cache_engine.py
+++ vllm/worker/cache_engine.py
@@ -32,17 +32,19 @@ class CacheEngine:
         self.parallel_config = parallel_config
 
         self.head_size = model_config.get_head_size()
-        if getattr(model_config.hf_config, "model_type", "") == "evomistral":
+        print("test:", model_config.hf_config.model_type)
+        if model_config.hf_config.model_type == "evomistral":
             self.num_layers = model_config.get_num_hops(parallel_config)
             logger.info(f"Detected EvoMistral (num_hops: {self.num_layers})")
         else:
             self.num_layers = model_config.get_num_layers(parallel_config)
+        print("test:", self.num_layers)
         self.num_heads = model_config.get_num_kv_heads(parallel_config)
 
         self.block_size = cache_config.block_size
         self.num_gpu_blocks = cache_config.num_gpu_blocks
         self.num_cpu_blocks = cache_config.num_cpu_blocks
-
+        
         if cache_config.cache_dtype == "auto":
             self.dtype = model_config.dtype
         else:
